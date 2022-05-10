cb046a80a522e9a112275db54bc88dc5001db638
MysaelR <90765797+MysaelR@users.noreply.github.com>
2022-05-10 16:54:48 -0300

    atualizando

--- clinica-adm/src/components/menu/index.tsx
+++ clinica-adm/src/components/menu/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import * as Nav from './style';
-import Logo2 from '../../assets/logo climed 2.png';
-
+import Logo2 from '../../assets/menu/logo climed 2.png';
+import { House, Pill, AddressBook } from "phosphor-react";
 
 export default function Menu() {
 
@@ -9,8 +9,22 @@ export default function Menu() {
 
     return (
         <Nav.MenuContainer>
-            <Nav.ImageLogo src={Logo2} id="logo"/>
+            <Nav.ImageLogo src={Logo2} id="logo" />
 
-        </Nav.MenuContainer>
+            <Nav.MenuOptions>
+                <Nav.Option>
+
+                    <House size={32} id="image-option" />
+
+                    <Nav.DescriptionOption>
+                        Home
+                    </Nav.DescriptionOption>
+                </Nav.Option>
+
+                <Pill size={32} />
+                <AddressBook size={32} />
+            </Nav.MenuOptions>
+
+        </Nav.MenuContainer >
     );
 }
--- clinica-adm/src/components/menu/style.ts
+++ clinica-adm/src/components/menu/style.ts
@@ -3,30 +3,32 @@ import styled, {css} from "styled-components";
 import Logo from '../../assets/logo climed.png';
 
 
+export const DescriptionOption = styled.p`
+    margin-left: 1vw;
+    display: none;
+   
+`
 
 export const MenuContainer = styled.div`
     height: 100vh;
     width: 3vw;
     background-color: #1DB954;
 
-
-
-    
-
     :hover{
         transition: 0.05s ease-in;
         width: 10vw;
-        
-        #logo{
-            width:5vw;
-            margin: 1vw;
 
-
+       ${DescriptionOption}{
+            display: block;
         }
-
+        
         
     }
 `
+
+export const Business = styled.p`
+`
+
 export const ImageLogo = styled.img`
     padding: 5%;
     width: 90%;
@@ -34,5 +36,27 @@ export const ImageLogo = styled.img`
 `
 
 export const MenuOptions = styled.div`
-    
+    margin-top: 5vh;
+    display: grid;
+
+
+    #image-option{
+        margin-left: 5px;
+    }
+`
+
+
+
+
+export const Option = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 1vh 0;
+
+
+    :hover{
+        //background-color: #229c4d;
+        background-color: #2feb71;
+        opacity: 0.5;
+    }
 `
